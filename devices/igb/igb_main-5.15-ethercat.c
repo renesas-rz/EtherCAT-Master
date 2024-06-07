f7d2c053bc0d8eed0fecadd68a68f9b503b824fd
Bjarne von Horn <vh@igh.de>
2024-06-07 11:20:03 +0200

    Fix igb backports from 1.6

--- devices/igb/igb_main-5.15-ethercat.c
+++ devices/igb/igb_main-5.15-ethercat.c
@@ -8112,7 +8112,7 @@ static int igb_poll(struct napi_struct *napi, int budget)
 	bool clean_complete = true;
 	int work_done = 0;
 
-	if (get_ecdev(q_vector->adapter)) {
+	if (q_vector->adapter->ecdev) {
 		return -EBUSY;
 	}
 
